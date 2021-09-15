9b8de6f5c91b9ccfb620381c0f6e6eee16eb1912
Berina Sanjta <berina@sanjta.org>
2021-09-15 12:05:35 +0200

    Adding folder ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this line
+
+    //One line comment in JS
+
+    /* multi
+    line 
+    comment 
+    in JS
+    */
+
+    //Only change code above this line
+}
+
+console.log (main());
+module.exports = main;
